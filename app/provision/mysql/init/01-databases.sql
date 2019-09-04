e3d6c76d06eafa732428d13daaa163011a2edd31
Jackson Nightingale <jackson@yourcloudtelco.com.au>
2019-09-05 09:40:51 +1000

    first commit

---
+++ app/provision/mysql/init/01-databases.sql
@@ -0,0 +1,8 @@
+-- create databases
+CREATE DATABASE IF NOT EXISTS calldetails;
+CREATE DATABASE IF NOT EXISTS cdr;
+CREATE DATABASE IF NOT EXISTS coredb;
+
+-- create root user and grant rights
+CREATE USER 'arena'@'localhost' IDENTIFIED BY 'root';
+GRANT ALL ON *.* TO 'arena'@'%';
