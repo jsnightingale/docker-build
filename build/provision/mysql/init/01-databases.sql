1acbf1a8d055da7e35bd16637c8a01d25685b2bd
Jackson Nightingale <jackson@yourcloudtelco.com.au>
2019-09-18 16:30:51 +1000

    Set up build folder instead of app

--- build/provision/mysql/init/01-databases.sql
+++ build/provision/mysql/init/01-databases.sql
@@ -1,6 +1,7 @@
 -- create databases
 CREATE DATABASE IF NOT EXISTS calldetails;
 CREATE DATABASE IF NOT EXISTS cdr;
+CREATE DATABASE IF NOT EXISTS 2talk;
 CREATE DATABASE IF NOT EXISTS coredb;
 
 -- create root user and grant rights
